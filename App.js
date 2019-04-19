ae4d5e37bfaae2ec2622e68c0bd78be1eb6c95a0
alshin <alshin@126.com>
2019-04-19 20:20:59 +0800

    modfiy app

--- App.js
+++ App.js
@@ -2,161 +2,163 @@ import React from 'react';
 import { Button, Image, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // 1.0.0-beta.27
 
-class LogoTitle extends React.Component {
-  render() {
-    return (
-       <Text>test</Text>
-    );
-  }
-}
-
-class HomeScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    const params = navigation.state.params || {};
-
-    return {
-      headerTitle: <LogoTitle />,
-      headerLeft: (
-        <Button
-          onPress={() => navigation.navigate('MyModal')}
-          title="Info"
-          color="#fff"
-        />
-      ),
-      headerRight: (
-        <Button onPress={params.increaseCount} title="+1" color="#fff" />
-      ),
-    };
-  };
-
-  componentWillMount() {
-    this.props.navigation.setParams({ increaseCount: this._increaseCount });
-  }
-
-  state = {
-    count: 0,
-  };
-
-  _increaseCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Text>Count: {this.state.count}</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'First Details',
-            });
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    const { params } = navigation.state;
-
-    return {
-      title: params ? params.otherParam : 'A Nested Details Screen',
-      /* These values are used instead of the shared configuration! */
-      headerStyle: {
-        backgroundColor: navigationOptions.headerTintColor,
+class Find extends Component {
+  static navigationOptions = {
+      tabBarLabel: '发现',
+      tabBarIcon: ({ focused }) =>
+      {
+          if (focused) {
+              <Text>发现界面</Text>
+          }
+          return (
+              <Text>界面</Text>
+          );
       },
-      headerTintColor: navigationOptions.headerStyle.backgroundColor,
-    };
   };
 
   render() {
-    /* 2. Read the params from the navigation state */
-    const { params } = this.props.navigation.state;
-    const itemId = params ? params.itemId : null;
-    const otherParam = params ? params.otherParam : null;
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Update the title"
-          onPress={() =>
-            this.props.navigation.setParams({ otherParam: 'Updated!' })}
-        />
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
+      return (
+          <View style={styles.container}>
+              <Text>发现界面</Text>
+          </View>
+      );
   }
 }
+class Me extends Component {
+  static navigationOptions = {
+      tabBarLabel: '我的',
 
-class ModalScreen extends React.Component {
+      tabBarIcon: ({ focused }) =>
+      {
+          if (focused) {
+              <Text>我的界面</Text>
+          }
+          return (
+              <Text>界面</Text>
+          );
+      },
+  };
+
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-        <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Dismiss"
-        />
-      </View>
-    );
+      return (
+          <View style={styles.container}>
+              <Text>我的界面</Text>
+          </View>
+      );
   }
 }
 
-const MainStack = createStackNavigator(
+class Tong extends Component {
+  static navigationOptions = {
+      tabBarLabel: '通讯录',
+      tabBarIcon: ({ focused }) =>
+      {
+          if (focused) {
+              <Text>通讯录界面</Text>
+          }
+          return (
+              <Text>通讯录</Text>
+          );
+      },
+  };
+
+  render() {
+      return (
+          <View style={styles.container}>
+              <Text>通讯录界面</Text>
+          </View>
+      );
+  }
+}
+
+class Chat extends Component {
+  static navigationOptions = {
+      tabBarLabel: '聊天',
+
+      tabBarIcon: ({ focused }) =>
+      {
+          if (focused) {
+              return (
+                  <Text>聊天界面</Text>
+              );
+          }
+          return (
+                <Text>聊天</Text>
+          );
+      },
+  };
+
+  render() {
+      return (
+          <View style={styles.container}>
+              <Text>聊天界面</Text>
+          </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+  }
+});
+
+export const MyApp = TabNavigator(
   {
-    Home: {
-      screen: HomeScreen,
-    },
-    Details: {
-      screen: DetailsScreen,
-    },
+      Chat: {
+          screen: Chat,
+      },
+      Tong: {
+          screen: Tong,
+      },
+      Find: {
+          screen: Find,
+      },
+      Me: {
+          screen: Me,
+      },
+  },
+  {
+      tabBarOptions: {
+          activeTintColor: '#4BC1D2',
+          inactiveTintColor: '#000',
+          showIcon: true,
+          showLabel: true,
+          upperCaseLabel: false,
+          pressColor: '#788493',
+          pressOpacity: 0.8,
+          style: {
+              backgroundColor: '#fff',
+              paddingBottom: 0,
+              borderTopWidth: 0.5,
+              borderTopColor: '#ccc',
+          },
+          labelStyle: {
+              fontSize: 12,
+              margin: 1
+          },
+          indicatorStyle: {height: 0},
+      },
+      tabBarPosition: 'bottom',
+      swipeEnabled: true,
+      animationEnabled: false,
+      lazy: true,
+      backBehavior: 'none',
+});
+
+export const AppNav = createStackNavigator(
+  {
+    Home: { screen: MyApp },
   },
   {
     initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
   }
 );
 
-const RootStack = createStackNavigator(
-  {
-    Main: {
-      screen: MainStack,
-    },
-    MyModal: {
-      screen: ModalScreen,
-    },
-  },
-  {
-    mode: 'card',
-    headerMode: 'none',
-  }
-);
-
-const AppContainer = createAppContainer(RootStack);
+const AppContainer = createAppContainer(AppNav);
 
 export default class App extends React.Component {
   render() {
